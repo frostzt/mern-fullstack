6e16636fde4ed15eb410a74f6c8aa0efe5905880
frostz <aidenfrostbite@gmail.com>
2021-01-13 19:31:53 +0530

    Implemented user signup/login and auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('./../../models/User');
@@ -23,7 +25,7 @@ router.post(
     // Check and validate the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
@@ -32,7 +34,9 @@ router.post(
       // Check if the user exists
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists!' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists!' }] });
       }
 
       // Get users gravatar
@@ -55,7 +59,21 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send('User registered!');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err);
       res.status(500).send("Server error! We're working on it!");
